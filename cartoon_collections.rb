6416946f69a3548cfa3bf3a090f21ef8579ff2d9
Oneil Williams <oneilswilliams@gmail.com>
2019-08-21 01:23:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |d,index|
+    puts " #{index+1} #{d}"
+  end  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  summon = calls.map do |c|
+    "#{c.capitalize}!"
+  end
+  summon 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |c| 
+    c.size > 4 
+  end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  foods.find do |f|
+    f == cheese_types
+  end 
+  foods
+end 
+
